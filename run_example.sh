5b15380df1f3a2f0ba03311829ec9330f8c0cc17
franec94 <franec94@gmail.com>
2020-10-24 01:33:35 +0200

    Updates works on server.

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-data_path="../../../../results/23-10-2020/1603410154-248962/"
-logging_root="../results/"
+date_timestampe_partial_path="23-10-2020/1603410154-248962"
+
+data_path="../../../../results/${date_timestampe_partial_path}"
+logging_root="../results/${date_timestampe_partial_path}"
 
 chmod u+x analysis-single-run
 python3 analysis-single-run \
