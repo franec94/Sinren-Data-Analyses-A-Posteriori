cc4b03a5dc94e763a7e58bb1374a85035477b8d4
franec94 <franec94@gmail.com>
2020-10-24 01:05:56 +0200

    Updates seem to work on server.

--- run_example.sh
+++ run_example.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
+data_path="../../../../results/23-10-2020/1603410154-248962/"
+logging_root="../results/"
+
 chmod u+x analysis-single-run
-python3 analysis-single-run
+python3 analysis-single-run \
+  --data_path ${data_path} \
+  --logging_root ${logging_root}
 
 exit 0
 
