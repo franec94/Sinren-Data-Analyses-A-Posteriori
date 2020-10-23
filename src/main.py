394aaa682db95948f3a4565361585598288f2c57
franec94 <franec94@gmail.com>
2020-10-23 16:22:11 +0200

    Started implementations.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from utils.libs import *
+
+
+def main():
+    print(f"Hello World! From {sys.argv[0]}")
+    pass
+
+if __name__ == "__main__":
+    main()
+    pass
---
+++ src/utils/libs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+ # -*- coding: utf-8 -*-
+
+from __future__ import print_function
+from __future__ import division
+
+# Standard Library, plus some Third Party Libraries
+
+import logging
+from pprint import pprint
+from PIL import Image
+from tqdm import tqdm
+from typing import Union, Tuple
+
+import configargparse
+from functools import partial
+
+import collections
+import copy
+import datetime
+import functools
+import h5py
+import math
+import operator
+import os
+import random
+import shutil
+import sys
+import re
+import time
+# import visdom
+
+# Data Science and Machine Learning Libraries
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.style.use('ggplot')
+
+import numpy as np
+import pandas as pd
+import sklearn
